b171d4e4eadcb0ea10c835d316f19e8a38e1e224
Alexandre Leconte <aleconte@dwightstudio.fr>
2024-05-06 12:09:22 +0200

    Amélioration de la recherche simplifiée

--- src/main/java/fr/dwightstudio/connect4/search/SimpleSearchThread.java
+++ src/main/java/fr/dwightstudio/connect4/search/SimpleSearchThread.java
@@ -16,10 +16,17 @@ public class SimpleSearchThread extends SolverSearchThread {
         if (depth > MAX_RECURSIVITY) {
             if (state.isWinningState()) {
                 return ((GameState.FLAT_LENGTH + 1 - state.getNbMoves()) / 2);
-            } else if (state.computeWinningPositions() != 0) {
-                return ((GameState.FLAT_LENGTH + 1 - state.getNbMoves()) / 2) - 2;
             } else {
-                return 0;
+                long winingPositions = state.computeWinningPositions();
+
+                if (winingPositions != 0) {
+                    if ((winingPositions & (winingPositions - 1)) == 0) {
+                        return ((GameState.FLAT_LENGTH + 1 - state.getNbMoves()) / 2) - 2;
+                    }
+                    return ((GameState.FLAT_LENGTH + 1 - state.getNbMoves()) / 2) - 4;
+                } else {
+                    return 0;
+                }
             }
         }
 
